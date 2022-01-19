1877d453b83ea5dc191206681ab5f4cf642a182d
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-01-20 07:49:07 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -25,8 +25,7 @@ const defaultMenu = {
 ┃
 ┃⬡ Uptime: *%uptime (%muptime)*
 ┃⬡ Database: %rtotalreg dari %totalreg
-┃⬡ Github:
-┃⬡ https://github.com/AlyaaXd/rfkbot
+┃⬡ Github:https://github.com/Putbotz
 ┃
 ┗━━━━━━⬣`.trimStart(),
   header: '┏━━〔 %category 〕━⬣',
@@ -240,11 +239,10 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 ┃⬡ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
 ┃⬡ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 ┃
-┃⬡ Script by Alyaa
-┃⬡ Github Name : AlyaaXd
-┃⬡ Yt : AlyaaXd
-┃⬡ Run bot : Heroku
-┃⬡ Tipe SC rfkbot
+┃⬡ Script by Putbotz
+┃⬡ Github Name : Putbotz
+┃⬡ Yt : Putbotz
+┃⬡ Run bot : Heroku/Railway
 ┗━━━━━━━━⬣`.trim(),
           "buttonText": "Klik Disini",
           "listType": "SINGLE_SELECT",
