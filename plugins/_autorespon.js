04d5189b7f4a98c50aefeaec84964aa648deb870
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-01-20 11:31:56 +0800

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -1,41 +1,25 @@
 let fs = require('fs')
-let fetch = require('node-fetch')
 let handler = m => m
 
-handler.all = async function (m, { isBlocked }) {
+handler.all = async function (m, { conn, isBlocked }) {
 
-    if (isBlocked) return
-    if (m.isBaileys) return
-    if (m.chat.endsWith('broadcast')) return
-    let setting = db.data.settings[this.user.jid]
+    if (isBlocked || m.fromMe || m.chat.endsWith('broadcast')) return
+    let set = db.data.settings[this.user.jid]
     let { isBanned } = db.data.chats[m.chat]
     let { banned } = db.data.users[m.sender]
+    
 
-    // ketika ditag
-    try {
-        if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
-            await this.send2Button(m.chat,
-                isBanned ? `${namabot} tidak aktif` : banned ? 'kamu dibanned' : `${namabot} disini, ada yang bisa saya bantu?`,
-                footer,
-                isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
-                isBanned ? '.unban' : banned ? '.owner' : '.?',
-                m.isGroup ? 'Ban' : isBanned ? 'Unban' : 'Donasi',
-                m.isGroup ? '.ban' : isBanned ? '.unban' : '.donasi', m)
-        }
-    } catch (e) {
-        return
-    }
 
-    // salam
-    let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم/aslm)/i
+    //salam
+    let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم|aslm)/i
     let isSalam = reg.exec(m.text)
     if (isSalam && !m.fromMe) {
         m.reply(`وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n_wa\'alaikumussalam wr.wb._`)
     }
 
     // backup db
-    if (setting.backup) {
-        if (new Date() * 1 - setting.backupDB > 1000 * 60 * 60) {
+    if (set.backup) {
+        if (new Date() * 1 - set.backupTime > 1000 * 60 * 60) {
             let d = new Date
             let date = d.toLocaleDateString('id', {
                 day: 'numeric',
@@ -45,29 +29,20 @@ handler.all = async function (m, { isBlocked }) {
             await global.db.write()
             this.reply(global.owner[0] + '@s.whatsapp.net', `Database: ${date}`, null)
             this.sendFile(global.owner[0] + '@s.whatsapp.net', fs.readFileSync('./database.json'), 'database.json', '', 0, 0, { mimetype: 'application/json' })
-            setting.backupDB = new Date() * 1
+            set.backupTime = new Date() * 1
         }
     }
 
     // update status
-    if (new Date() * 1 - setting.status > 1000) {
-        let _uptime = process.uptime() * 1000
-        let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | ${namabot} by ${namalu}`).catch(_ => _)
-        setting.status = new Date() * 1
+    if (set.autoupdatestatus) {
+        if (new Date() * 1 - set.status > 1000) {
+            let _uptime = process.uptime() * 1000
+            let uptime = conn.clockString(_uptime)
+            await this.setStatus(`Aktif selama ${uptime} | Mode: ${set.self ? 'Private' : set.group ? 'Hanya Grup' : 'Publik'} | stikerinbot oleh ariffb`).catch(_ => _)
+            set.status = new Date() * 1
+        }
     }
 
 }
 
-module.exports = handler
-
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
-
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)]
-}
+module.exports = handler 
